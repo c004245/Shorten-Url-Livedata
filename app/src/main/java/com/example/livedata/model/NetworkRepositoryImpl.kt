5dab8477976efefaa7964a09481f184994113585
hyunwook <c004245@naver.com>
2019-07-30 11:02:09 +0900

    naver shorten url API 요청 후 result 값 얻기

--- app/src/main/java/com/example/livedata/model/NetworkRepositoryImpl.kt
+++ app/src/main/java/com/example/livedata/model/NetworkRepositoryImpl.kt
@@ -1,3 +1,13 @@
 package com.example.livedata.model
 
-class NetworkRepositoryImpl(private val: api)
+import com.example.livedata.api.Api
+import io.reactivex.Single
+
+class NetworkRepositoryImpl(private val api: Api): Repository {
+    override fun getShortenUrl(url: String): Single<ShortenUrl> {
+        return api.shorturl(url)
+            .map { shortenUrlResponse ->
+                shortenUrlResponse.result
+            }
+    }
+}
